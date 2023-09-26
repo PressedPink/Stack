8d8dd6616a95e83e9d2ad41de040c37534e30fc5
BuildTools <isaactexting@gmail.com>
2023-09-26 15:04:40 -0500

    Project Files

---
+++ StackMain/Stack/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for ToolOwnershipTracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'Stack.settings')
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'project_name'))
+sys.path.insert(0, root_path)
+
+application = get_wsgi_application()
