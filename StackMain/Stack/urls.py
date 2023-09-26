72e0940fe7f2194a8de3cdcbc70ca56526523248
BuildTools <isaactexting@gmail.com>
2023-09-26 16:19:29 -0500

    Bootstrap Inclusion

---
+++ StackMain/Stack/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import include
+from django.views.generic import RedirectView
+
+import base
+
+from Stack.views import Login
+
+urlpatterns = [
+    path('', include('pwa.urls')),
+    path('', Login.as_view(), name='login'),
+    path('admin/', admin.site.urls),
+]
